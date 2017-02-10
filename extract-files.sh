1eb841dbba714f55d3138fe9ad65bddf2242fb9c
Terence Tan <~@calroth.net>
2017-02-10 21:11:22 +1100

    libra: Custom deodex routines for Android L APKs
    The standard LineageOS deodex routines (oat2dex) uses baksmali, which
    can't deodex ART files from Android L. Replace that routine with one
    that uses oat2dex from https://github.com/testwhat/SmaliEx
    
    Only necessary for cm-13.0. Can be reverted for cm-14.1 as we'll
    presumably be using Android N APKs by that time.
    
    Change-Id: I978a442c35713ba0e9dc5c9fbbca4861f9630c71

--- extract-files.sh
+++ extract-files.sh
@@ -39,6 +39,82 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
+# Overridden version of oat2dex for libra below...
+
+#
+# oat2dex:
+#
+# $1: extracted apk|jar (to check if deodex is required)
+# $2: odexed apk|jar to deodex
+# $3: source of the odexed apk|jar
+#
+# Convert apk|jar .odex in the corresposing classes.dex
+#
+function oat2dex() {
+    local CM_TARGET="$1"
+    local OEM_TARGET="$2"
+    local SRC="$3"
+    local TARGET=
+    local OAT=
+
+    if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ] || [ -z "$OAT2DEXJAR" ]; then
+        export BAKSMALIJAR="$CM_ROOT"/vendor/cm/build/tools/smali/baksmali.jar
+        export SMALIJAR="$CM_ROOT"/vendor/cm/build/tools/smali/smali.jar
+        export OAT2DEXJAR="$CM_ROOT"/device/xiaomi/libra/oat2dex.jar
+    fi
+
+    # Extract existing boot.oats to the temp folder
+    if [ -z "$ARCHES" ]; then
+        echo "Checking if system is odexed and extracting boot.oats, if applicable. This may take a while..."
+        for ARCH in "arm64" "arm" "x86_64" "x86"; do
+            if get_file "system/framework/$ARCH/boot.oat" "$TMPDIR/boot_$ARCH.oat" "$SRC"; then
+                ARCHES+="$ARCH "
+            fi
+        done
+    fi
+
+    if [ -z "$ARCHES" ]; then
+        FULLY_DEODEXED=1 && return 0 # system is fully deodexed, return
+    fi
+
+    if [ ! -f "$CM_TARGET" ]; then
+        return;
+    fi
+
+    if grep "classes.dex" "$CM_TARGET" >/dev/null; then
+        return 0 # target apk|jar is already odexed, return
+    fi
+
+    for ARCH in $ARCHES; do
+        BOOTOAT="$TMPDIR/boot_$ARCH.oat"
+
+        local OAT="$(dirname "$OEM_TARGET")/oat/$ARCH/$(basename "$OEM_TARGET" ."${OEM_TARGET##*.}").odex"
+        local OAT2="$(dirname "$OEM_TARGET")/$ARCH/$(basename "$OEM_TARGET" ."${OEM_TARGET##*.}").odex"
+
+        if get_file "$OAT" "$TMPDIR" "$SRC"; then
+            java -jar "$BAKSMALIJAR" -x -o "$TMPDIR/dexout" -c "$BOOTOAT" -d "$TMPDIR" "$TMPDIR/$(basename "$OAT")"
+        elif get_file "$OAT2" "$TMPDIR" "$SRC"; then
+		    # Special version which runs oat2dex to deodex the OAT file
+			# Regular baksmali can't handle art version 45 files (from Android L)
+            java -jar "$OAT2DEXJAR" boot "$BOOTOAT"
+            java -jar "$OAT2DEXJAR" "$TMPDIR/$(basename "$OAT2")" "$TMPDIR/odex"
+            java -jar "$BAKSMALIJAR" -x -o "$TMPDIR/dexout" -c "$BOOTOAT" -d "$TMPDIR" "$TMPDIR/$(basename "$OAT2" ".odex").dex"
+        elif [[ "$CM_TARGET" =~ .jar$ ]]; then
+            # try to extract classes.dex from boot.oat for framework jars
+            echo "(2)" java -jar "$BAKSMALIJAR" -x -o "$TMPDIR/dexout" -c "$BOOTOAT" -d "$TMPDIR" -e "/$OEM_TARGET" "$BOOTOAT"
+            java -jar "$BAKSMALIJAR" -x -o "$TMPDIR/dexout" -c "$BOOTOAT" -d "$TMPDIR" -e "/$OEM_TARGET" "$BOOTOAT"
+        else
+            continue
+        fi
+
+        java -jar "$SMALIJAR" "$TMPDIR/dexout" -o "$TMPDIR/classes.dex" && break
+    done
+
+    rm -rf "$TMPDIR/dexout"
+}
+
+# ...Overridden version of oat2dex for libra above
+
 if [ $# -eq 0 ]; then
   SRC=adb
 else
